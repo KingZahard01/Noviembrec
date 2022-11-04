34cd95f7035bf817df36836a407d6903909ba3bd
KingZahard01 <juanjose10312@gmail.com>
2022-11-04 08:34:09 -0700

    Agregar archivos de proyecto.

---
+++ Noviembrex.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Noviembrex.Web.Controllers {
+    public class HomeController : Controller {
+
+        // Acciones de controlador
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult Bienvenida() {
+            return View();
+        }
+
+    }
+}
